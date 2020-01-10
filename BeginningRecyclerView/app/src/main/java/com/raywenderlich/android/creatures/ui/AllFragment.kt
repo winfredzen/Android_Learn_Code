146be6e3c1d9a0e206ec5ab818e386519c94e0e0
wangzhen <wangzhen@curefun.com>
2020-01-10 18:14:18 +0800

    嵌套RecyclerView

--- BeginningRecyclerView/app/src/main/java/com/raywenderlich/android/creatures/ui/AllFragment.kt
+++ BeginningRecyclerView/app/src/main/java/com/raywenderlich/android/creatures/ui/AllFragment.kt
@@ -32,6 +32,7 @@ package com.raywenderlich.android.creatures.ui
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
@@ -44,8 +45,9 @@ import kotlinx.android.synthetic.main.fragment_all.*
 
 class AllFragment : Fragment() {
 
-  //private val adpater = CreatureAdpater(CreatureStore.getCreatures().toMutableList())
-  private val adpater = CreatureWithFoodAdpater(CreatureStore.getCreatures().toMutableList())
+//  private val adpater = CreatureAdpater(CreatureStore.getCreatures().toMutableList())
+//  private val adpater = CreatureWithFoodAdpater(CreatureStore.getCreatures().toMutableList())
+private val adpater = CreatureCardAdpater(CreatureStore.getCreatures().toMutableList())
 
   companion object {
     fun newInstance(): AllFragment {
@@ -60,7 +62,10 @@ class AllFragment : Fragment() {
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
 
-    creatureRecyclerView.layoutManager = LinearLayoutManager(activity)
+//    creatureRecyclerView.layoutManager = LinearLayoutManager(activity)
+
+
+    creatureRecyclerView.layoutManager = GridLayoutManager(activity, 2, GridLayoutManager.VERTICAL, false)
     creatureRecyclerView.adapter = adpater
 
 
---
+++ BeginningRecyclerView/app/src/main/java/com/raywenderlich/android/creatures/ui/CreatureCardAdpater.kt
@@ -0,0 +1,86 @@
+package com.raywenderlich.android.creatures.ui
+
+import android.content.Context
+import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.graphics.drawable.Drawable
+import android.support.v4.content.ContextCompat
+import android.support.v7.graphics.Palette
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+import com.raywenderlich.android.creatures.R
+import com.raywenderlich.android.creatures.app.inflate
+import com.raywenderlich.android.creatures.model.Creature
+import kotlinx.android.synthetic.main.list_item_creature.view.*
+import kotlinx.android.synthetic.main.list_item_creature.view.creatureImage
+import kotlinx.android.synthetic.main.list_item_creature.view.nickName
+import kotlinx.android.synthetic.main.list_item_creature_card.view.*
+
+class CreatureCardAdpater(private val creatures: MutableList<Creature>) : RecyclerView.Adapter<CreatureCardAdpater.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+        //创建ViewHolder，这是使用的是ViewGroup上的一个扩展方法
+        /*
+        fun ViewGroup.inflate(@LayoutRes layoutRes: Int, attachToRoot: Boolean = false): View {
+            return LayoutInflater.from(context).inflate(layoutRes, this, attachToRoot)
+        }
+         */
+        return ViewHolder(parent.inflate(R.layout.list_item_creature_card))
+
+    }
+
+    override fun getItemCount() = creatures.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        //在这里ViewHolder绑定数据
+        holder.bind(creatures[position])
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
+
+        private lateinit var creature: Creature
+
+        init {
+            //注册点击事件
+            itemView.setOnClickListener(this)
+        }
+
+        //绑定数据
+        fun bind(creature: Creature) {
+            this.creature = creature
+            val context = itemView.context
+            val imageResource = context.resources.getIdentifier(creature.uri, null, context.packageName)
+            itemView.creatureImage.setImageResource(imageResource)
+            itemView.nickName.text = creature.nickname
+
+            setBackgroundColors(context, imageResource)
+        }
+
+        //实现点击事件方法
+        override fun onClick(view: View) {
+            val context = view.context
+            val intent = CreatureActivity.newIntent(context, creature.id)
+            context.startActivity(intent)
+        }
+
+        private fun setBackgroundColors(context: Context, imageResource: Int) {
+            val image = BitmapFactory.decodeResource(context.resources, imageResource)
+            Palette.from(image).generate { palette ->  
+                val backgroundColor = palette.getDominantColor(ContextCompat.getColor(context, R.color.colorPrimaryDark))
+                itemView.creatureCard.setBackgroundColor(backgroundColor)
+                itemView.nicknameHolder.setBackgroundColor(backgroundColor)
+                val textColor = if (isColorDark(backgroundColor)) Color.WHITE else Color.BLACK
+                itemView.nickName.setTextColor(textColor)
+            }
+        }
+
+        private fun isColorDark(color: Int): Boolean {
+            val darkness = 1 - (0.299 * Color.red(color) + 0.587 * Color.green(color) + 0.114 * Color.blue(color)) / 255
+            return darkness >= 0.5
+        }
+
+    }
+
+}
